ea104727e048546616de2041b7ae364708c2204a
mickael Ndinga <ndingamickael@gmail.com>
2023-02-01 09:43:27 +0100

    fix create module

--- core/Console/ApplicationCommands/Create/Create.php
+++ core/Console/ApplicationCommands/Create/Create.php
@@ -42,10 +42,13 @@ abstract class Create extends AbstractCommand
         $fileModel = file_get_contents(dirname(__DIR__) . "/models/{$this->createType}.model");
         if (!isset($this->variables['name'])) {
             $name = ucfirst($input->argument('name'));
+            if (!$input->option('real')) {
+                if (!str_ends_with($name, $this->suffix)) {
+                    $name .= $this->suffix;
+                }
+            }
         } else {
-            $name = $this->variables['name'];
-        }
-        if (!$input->option('real')) {
+            $name = ucfirst($this->variables['name']);
             if (!str_ends_with($name, $this->suffix)) {
                 $name .= $this->suffix;
             }
